4942c581dd8f3514fb693c6580efdbc09a78f7be
Marketa Sovova <M1482@student.jamk.fi>
2019-10-09 03:39:50 +0300

    MainScreen style

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,45 +1,67 @@
 import React from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { Ionicons } from '@expo/vector-icons';
 
 export default class MainScreen extends React.Component {
   static navigationOptions = {
     headerTitle: 'Photo App',
     headerTintColor: 'white',
+    headerTitleStyle: { 
+      textAlign:"center", 
+      flex:1 
+    },
     headerStyle: {
-      backgroundColor: 'blue'
+      backgroundColor: 'black'
     }
   }
     render() {
         return (
             <View style={styles.container}>
-                <Text>Hello {this.props.navigation.getParam('username')}!</Text>    
-
-                <Button style={styles.text}
-                    title="Take a photo"
-                    onPress={() => this.props.navigation.navigate('Camera')}
-                />
-                <Button style={styles.text}
-                    title="Show gallery"
-                    onPress={() => this.props.navigation.navigate('Gallery')}
-                />
-                <Button style={styles.text}
-                    title="Go Back"
-                    onPress={() => this.props.navigation.goBack()} // TODO logout
-                />
+            {(this.props.navigation.getParam('username'))
+              ? <Text style={styles.text}>Hello {this.props.navigation.getParam('username')}!</Text>
+              : <Text style={styles.text}>Hello!</Text>}
+                <Ionicons name='md-camera' size={100}/>
+                <TouchableOpacity style={styles.button} activeOpacity={.7}
+                    onPress={() => this.props.navigation.navigate('Camera')}>
+                    <Text style={styles.buttonText}>Take a photo</Text>
+                </TouchableOpacity>
+                <Ionicons name='md-image' size={100}/>
+                <TouchableOpacity style={styles.button} activeOpacity={.7}
+                    onPress={() => this.props.navigation.navigate('Gallery')}>
+                    <Text style={styles.buttonText}>Show gallery</Text>
+                </TouchableOpacity>
             </View>
         )
     }
 }
 
-
 const styles = StyleSheet.create({
   text: {
-    margin: 30,
+    fontSize: 22,
+    fontWeight: 'bold',
+    marginTop: 50,
+    marginBottom: 40,
+  },
+  button: {
+    backgroundColor: '#f5958e',
+    elevation: 2,
+    height: 60,
+    width: 200,
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+    margin: 20,
+  },
+  buttonText: {
+    fontSize: 20, 
+    color: 'white', 
+    fontWeight: 'bold'
   },
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
   },
 });
