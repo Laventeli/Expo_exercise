c14a388e597b89c3623e097ca7dbb6206e607bfc
Marketa Sovova <M1482@student.jamk.fi>
2019-10-09 04:43:23 +0300

    image size fixed

--- screens/CameraScreen.js
+++ screens/CameraScreen.js
@@ -1,23 +1,26 @@
 import React from 'react';
-import { Image, Text, View, TouchableOpacity, Dimensions } from 'react-native';
+import { Image, Text, View, TouchableOpacity } from 'react-native';
 import * as Permissions from 'expo-permissions';
 import { Camera } from 'expo-camera';
-import { ThemeColors } from 'react-navigation';
 import * as FileSystem from 'expo-file-system';
+import { Ionicons } from '@expo/vector-icons';
 
 
 export default class CameraScreen extends React.Component {
   static navigationOptions = {
     headerTitle: 'Camera',
     headerTintColor: 'white',
+    headerTitleStyle: { 
+      textAlign:"center", 
+      flex:1 
+    },
     headerStyle: {
-      backgroundColor: 'green'
+      backgroundColor: 'black'
     }
   }
   state = {
     hasCameraPermission: null,
     type: Camera.Constants.Type.back,
-    imageuri: null,
     imageId: null,
     newImage: false,
   };
@@ -26,38 +29,27 @@ export default class CameraScreen extends React.Component {
     this.setState({ hasCameraPermission: status === 'granted' });
   }
 
-  componentWillUnmount() {
-    console.log('unmounting camerascreen');
-  }
-
-  async saveImage(uriphoto, id) {
-    console.log('fc saveImage - uriphoto:', uriphoto);
-    console.log('fc saveImage - id:', id);
+  async saveImage(photouri, id) {
     const dirUri = FileSystem.documentDirectory + 'photos/'
     await FileSystem.makeDirectoryAsync(dirUri, {intermediates: true });
     await FileSystem.moveAsync({
-                        from: uriphoto,
-                        to: `${FileSystem.documentDirectory}photos/Photo_${
-                        id
-                        }.jpg`});
-    alert('Photo saved')  
-    console.log('image saved, id is', id);                    
-    this.setState({newImage: true, imageId: id});  
-    console.log('image save2 (after setstat', this.state.imageId);                 
-      //let info = await FileSystem.getInfoAsync(this.state.imageuri); //TODO FileSystem.copyAsync - make a permanent copy
-
+      from: photouri,
+      to: `${dirUri}Photo_${id}.jpg`});
+    alert('Photo saved')                   
+    this.setState({newImage: true, imageId: id});                   
   }
  
   async snapPhoto() {      
     if (this.camera) {
       const options = { quality: 1, base64: false, fixOrientation: true, exif: true, skipProcessing: true};
       let photo = await this.camera.takePictureAsync(options);
-      photo.exif.Orientation = 1;  
-      // -------------
       this.saveImage(photo.uri, this.idGenerator())
      }
     }
 
+  /**
+   * Generates random 8-number/letter id
+   */
   idGenerator() {
       var S4 = function() {
          return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
@@ -65,17 +57,15 @@ export default class CameraScreen extends React.Component {
       return (S4()+S4());
   }
   
-
-
   render() {
     const { hasCameraPermission } = this.state;
     if (hasCameraPermission === null) {
       return <View />;
     } else if (hasCameraPermission === false) {
       return <Text>No access to camera</Text>;
-    } else if (this.state.newImage) {
+    } else if (this.state.newImage) { // after photo has been taken
       return <Image
-      style={{width: Dimensions.get('window').width, height: Dimensions.get('window').height}}
+      style={{flex: 1, width: null, height: null, resizeMode:'stretch'}}
       source={{uri: `${FileSystem.documentDirectory}photos/Photo_${
         this.state.imageId
         }.jpg`}}
@@ -106,17 +96,17 @@ export default class CameraScreen extends React.Component {
                 }}>
                 <Text style={{ fontSize: 18, marginBottom: 10, color: 'white' }}> Flip </Text>
               </TouchableOpacity>
-              <TouchableOpacity
+              <TouchableOpacity activeOpacity={.5}
                 style={{
-                  flex: 0.1,
+                  flex: 0.8,
                   alignSelf: 'flex-end',
                   alignItems: 'center',
+                  marginBottom: 30,
                 }}
                 onPress={() => {
-                  this.snapPhoto()
-                
+                  this.snapPhoto()               
                 }}>
-                <Text style={{ fontSize: 18, marginBottom: 10, color: 'white' }}> Snap </Text>
+                <Ionicons name='md-radio-button-on' size={75} color='white'/>
               </TouchableOpacity>
             </View>
           </Camera>
